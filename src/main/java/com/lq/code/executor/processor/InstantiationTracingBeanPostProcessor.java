055b70ee14a56b26d19fc095431b2da5aa6a301f
qi_liang <qi_liang_gz@163.com>
2019-05-07 05:35:58 +0800

    删除废弃代码

--- src/main/java/com/lq/code/executor/processor/InstantiationTracingBeanPostProcessor.java
+++ src/main/java/com/lq/code/executor/processor/InstantiationTracingBeanPostProcessor.java
@@ -41,7 +41,6 @@ public class InstantiationTracingBeanPostProcessor implements ApplicationListene
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         if(contextRefreshedEvent.getApplicationContext().getParent() == null){
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             //root application context 没有parent，他就是老大.
             //需要执行的逻辑代码，当spring容器初始化完成后就会执行该方法。
             //扫描实体类
