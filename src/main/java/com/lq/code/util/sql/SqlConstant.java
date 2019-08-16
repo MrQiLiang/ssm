c3935945e3a78110b2f773689ac036dd58f7165d
qi <qi_liang_gz@163.com>
2019-08-16 19:37:39 +0800

    新增常量

--- src/main/java/com/lq/code/util/sql/SqlConstant.java
+++ src/main/java/com/lq/code/util/sql/SqlConstant.java
@@ -7,10 +7,26 @@ package com.lq.code.util.sql;
  */
 public class SqlConstant {
 
+    /**
+     *  SQL 切割符
+     */
     public static final String SQL_SYMBOL = "_";
-
+    /**
+     *  数据库类型常量：mysql
+     */
     public static final String DB_TYPE_MYSQL = "mysql";
-
+    /**
+     * 数据库类型常量：oracle
+     */
     public static final String DB_TYPE_ORACLE = "oracle";
+
+    /**
+     * 常用SQL 关键字:FROM
+     */
+    public static final String FROM_SQL = "FROM ";
+    /**
+     * 常用SQL 关键字:order
+     */
+    public static final String ORDER_SQL="ORDER";
 
 }
