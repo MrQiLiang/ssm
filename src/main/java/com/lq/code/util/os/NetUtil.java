a8cfae723c42a488eb4b1a556e756eaacf6e0711
qi_liang <qi_liang_gz@163.com>
2020-03-04 07:23:33 +0800

    更新实体类

--- src/main/java/com/lq/code/util/os/NetUtil.java
+++ src/main/java/com/lq/code/util/os/NetUtil.java
@@ -1,12 +1,20 @@
 package com.lq.code.util.os;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 /** 获取计算机网络信息
  * Created by qi_liang on 2018/6/9.
  */
 public class NetUtil {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(NetUtil.class);
 
     public static String getIP(){
         String ip = null;
@@ -18,6 +26,33 @@ public class NetUtil {
             e.printStackTrace();
         }
         return  ip;
+    }
+
+    public static String getLocalIpAddr(){
+        Enumeration<NetworkInterface> networks = null;
+        try {
+            // 获取网卡设备
+            networks = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            LOGGER.info(e.getMessage());
+        }
+        InetAddress ip = null;
+        Enumeration<InetAddress> addrs;
+        // 遍历网卡设备
+        while (networks.hasMoreElements()){
+            addrs = networks.nextElement().getInetAddresses();
+            while (addrs.hasMoreElements()){
+                ip = addrs.nextElement();
+                if (ip != null && ip instanceof InetAddress && ip.isSiteLocalAddress()){
+                    if (ip.getHostAddress()==null || "".equals(ip.getHostAddress())){
+                        LOGGER.info("获取到的客户端内网ip为空，从配置文件读取本地ip。");
+                        return null;
+                    }
+                    return ip.getHostAddress();// 客户端ip
+                }
+            }
+        }
+        return null;
     }
 
 }
