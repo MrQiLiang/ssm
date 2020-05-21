c37a531675d71fc2d3ca87e76bbfcd3268ecedd7
qi <qi_liang_gz@163.com>
2020-05-21 12:00:02 +0800

    修改注解

--- src/main/java/com/lq/api/service/SysFileService.java
+++ src/main/java/com/lq/api/service/SysFileService.java
@@ -3,6 +3,10 @@ package com.lq.api.service;
 import com.lq.code.service.BaseService;
 import com.lq.entity.SysFile;
 
+/**
+ *  文件实物类
+ * @author qi
+ */
 public interface SysFileService extends BaseService<SysFile>{
 
 
