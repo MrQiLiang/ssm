de74cce68deb91bb31e21ec1e370ea08b153aa47
qi <qi_liang_gz@163.com>
2020-10-11 01:29:15 +0800

    添加注解

--- src/main/java/com/lq/cms/web/sys/OssController.java
+++ src/main/java/com/lq/cms/web/sys/OssController.java
@@ -1,4 +1,15 @@
 package com.lq.cms.web.sys;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * oss 控制类
+ * @author qi
+ */
+@Controller
+@RequestMapping("/sys/oss")
 public class OssController {
+
+
 }
